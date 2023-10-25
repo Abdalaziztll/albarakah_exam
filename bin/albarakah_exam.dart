5ab100173047ba506259479e31c0bab69d4e1275
AbdalazizTll <abdalaziztll992010@gmail.com>
2023-10-25 03:57:34 -0700

    init point

---
+++ bin/albarakah_exam.dart
@@ -0,0 +1,11 @@
+void main() {
+  // ! TASK 3 : 10 Marks
+  // TODO : First of All Open lib >> model >> user_model.dart and do the task
+  // TODO : Then Open lib >> service >> sevice.dart and do the task
+  // TODO : Here Create an Object of Your class
+  // TODO : use Your instance login with your username and and any password and send your message
+  // TODO : then get your message from getMessage method
+
+  // ? Prefered to add Future.delay with 5 sec Duration between two method to ensure thet your token is arrived
+  
+}
---
+++ test/albarakah_exam_test.dart
@@ -0,0 +1,8 @@
+import 'package:albarakah_exam/albarakah_exam.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
